860827714be07ff94f4bbd86ebf9683384e648ea
Aaryan Chaudhary <aaryanchaudhary188@gmail.com>
2023-11-26 14:01:18 +1100

    added interactive buttons and result display

--- script.js
+++ script.js
@@ -1,42 +1,58 @@
+const rockBtn = document.querySelector(".rock");
+const paperBtn = document.querySelector(".paper");
+const scissorBtn = document.querySelector(".scissor");
+const div = document.querySelector(".gameResult");
+const result = document.createElement("p");
 const computerOpt = ["Rock", "Paper", "Scissor"];
+let computerSelection;
+rockBtn.addEventListener("click", () => {
+  const rock = rockBtn.value;
+  console.log(rockBtn.value);
+  computerSelection = getComputerChoice();
 
+  gamePlay(rock.toLowerCase(), computerSelection);
+});
+paperBtn.addEventListener("click", () => {
+  const paper = paperBtn.value;
+  console.log(paperBtn.value);
+  computerSelection = getComputerChoice();
+  gamePlay(paper.toLowerCase(), computerSelection);
+});
+scissorBtn.addEventListener("click", () => {
+  const scissor = scissorBtn.value;
+  console.log(scissorBtn.value);
+  computerSelection = getComputerChoice();
+  gamePlay(scissor.toLowerCase(), computerSelection);
+});
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
   console.log(computerOpt[randomNumber].toLowerCase());
   return computerOpt[randomNumber].toLowerCase();
 }
 
-let computerSelection = getComputerChoice();
-
 function gamePlay(playerSelection, computerSelection) {
   console.log(playerSelection, computerSelection);
 
   if (playerSelection === "rock" && computerSelection === "paper") {
-    console.log("You Lose! Paper beats Rock");
+    result.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    div.appendChild(result);
   } else if (playerSelection === "rock" && computerSelection === "scissor") {
-    console.log("You Win! Rock crushes Scissors");
+    result.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+    div.appendChild(result);
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    console.log("You Win! Paper beats Rock");
+    result.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+    div.appendChild(result);
   } else if (playerSelection === "paper" && computerSelection === "scissor") {
-    console.log("You lose! scissor cuts paper");
+    result.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    div.appendChild(result);
   } else if (playerSelection === "scissor" && computerSelection === "rock") {
-    console.log("You Lose! rock is broken by scissor");
+    result.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    div.appendChild(result);
   } else if (playerSelection === "scissor" && computerSelection === "paper") {
-    console.log("You Win! scissor cuts paper");
-  } else if (playerSelection !== "rock" || "scissor" || "paper") {
-    console.log(
-      "Invalid Input! Please enter either 'rock', 'scissor' or 'paper' only."
-    );
+    result.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+    div.appendChild(result);
   } else {
-    console.log("It's a tie");
+    result.textContent = `It's a Tie`;
+    div.appendChild(result);
   }
 }
-let playerSelection;
-function game() {
-  playerSelection = prompt(
-    "Enter your choice: rock/paper/scissors"
-  ).toLowerCase();
-  gamePlay(playerSelection, computerSelection);
-}
-
-game();
