900a14234134dcbff648d7bf0abac4800a013104
Aaryan Chaudhary <aaryanchaudhary188@gmail.com>
2023-11-26 14:35:54 +1100

    added score count and display with reset button

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const rockBtn = document.querySelector(".rock");
 const paperBtn = document.querySelector(".paper");
 const scissorBtn = document.querySelector(".scissor");
+const reset = document.querySelector(".reset");
 const div = document.querySelector(".gameResult");
 const result = document.createElement("p");
+const score = document.createElement("h1");
+const opponentScore = document.createElement("h1");
 const computerOpt = ["Rock", "Paper", "Scissor"];
 let computerSelection;
+let playerScore = 0;
+let compScore = 0;
 rockBtn.addEventListener("click", () => {
   const rock = rockBtn.value;
   console.log(rockBtn.value);
@@ -35,24 +40,42 @@ function gamePlay(playerSelection, computerSelection) {
 
   if (playerSelection === "rock" && computerSelection === "paper") {
     result.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    compScore++;
     div.appendChild(result);
   } else if (playerSelection === "rock" && computerSelection === "scissor") {
     result.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+    playerScore++;
     div.appendChild(result);
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     result.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+    playerScore++;
     div.appendChild(result);
   } else if (playerSelection === "paper" && computerSelection === "scissor") {
     result.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    compScore++;
     div.appendChild(result);
   } else if (playerSelection === "scissor" && computerSelection === "rock") {
     result.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    compScore++;
     div.appendChild(result);
   } else if (playerSelection === "scissor" && computerSelection === "paper") {
     result.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+    playerScore++;
     div.appendChild(result);
   } else {
     result.textContent = `It's a Tie`;
     div.appendChild(result);
   }
+  score.textContent = `Your Total score : ${playerScore}`;
+  opponentScore.textContent = `Computer total Score: ${compScore}`;
+  div.appendChild(score);
+  div.appendChild(opponentScore);
 }
+reset.addEventListener("click", () => {
+  result.textContent = ``;
+
+  playerScore = 0;
+  compScore = 0;
+  score.textContent = `Your Total score : ${playerScore}`;
+  opponentScore.textContent = `Computer total Score: ${compScore}`;
+});
